555f0c0b5d7c0785d8f33840330169d0cd64fa2b
18107 <18107@users.noreply.github.com>
2021-05-01 06:21:37 +1000

    Add project files.

---
+++ BeamMeUp/Command.cs
@@ -0,0 +1,32 @@
+﻿using PulsarPluginLoader.Chat.Commands.CommandRouter;
+using PulsarPluginLoader.Utilities;
+
+namespace BeamMeUp
+{
+    class Command : PublicCommand
+    {
+        public override string[] CommandAliases()
+        {
+            return new string[] { "beamMeUp" };
+        }
+
+        public override string Description()
+        {
+            return "Teleport the player back to the ship";
+        }
+
+        public override void Execute(string arguments, int SenderID)
+        {
+            if (PhotonNetwork.isMasterClient)
+            {
+                PLPlayer player = HelperMethods.GetPlayerFromPlayerID(SenderID);
+                int hubID = PLEncounterManager.Instance.PlayerShip.MyTLI.SubHubID;
+                player.photonView.RPC("NetworkTeleportToSubHub", PhotonTargets.All, new object[]
+                {
+                hubID,
+                0
+                });
+            }
+        }
+    }
+}
---
+++ BeamMeUp/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using PulsarPluginLoader;
+
+namespace BeamMeUp
+{
+    public class Plugin : PulsarPlugin
+    {
+        public override string Version => "0.0.0";
+
+        public override string Author => "18107";
+
+        public override string ShortDescription => "Allows players to teleport back to the ship at any time";
+
+        public override string Name => "Beam Me Up";
+
+        public override string HarmonyIdentifier()
+        {
+            return "mod.id107.beammeup";
+        }
+    }
+}
